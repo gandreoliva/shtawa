9b8424229a200ebcb16dca7995b695430e4b9896
André Oliva <gandreoliva@gmail.com>
2023-01-02 22:47:54 +0100

    include all

--- shtawa.f90
+++ shtawa.f90
@@ -4,17 +4,17 @@ module shtawa
     implicit none
     include "interp/type_grid.f90"
 contains
-    ! include "roots/bisection.f90"
-    ! include "roots/newton.f90"
-    ! include "roots/complex_newton.f90"
-    ! include "roots/secant.f90"
+    include "roots/bisection.f90"
+    include "roots/newton.f90"
+    include "roots/complex_newton.f90"
+    include "roots/secant.f90"
 
-    ! include "linalg/gauss.f90"
-    ! include "linalg/gaussjordan.f90"
-    ! include "linalg/determinant.f90"
-    ! include "linalg/inverse.f90"
-    ! include "linalg/tridiag_crout.f90"
-    ! include "linalg/complex_eigenval_power.f90"
+    include "linalg/gauss.f90"
+    include "linalg/gaussjordan.f90"
+    include "linalg/determinant.f90"
+    include "linalg/inverse.f90"
+    include "linalg/tridiag_crout.f90"
+    include "linalg/complex_eigenval_power.f90"
 
     include "combinatorics/ncartprod.f90"
     include "combinatorics/permutations.f90"
